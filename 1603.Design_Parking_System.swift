2b18e1dc47414f9b65ff82ac7eb1f8346efe3389
Andrew Po <ensimgen@gmail.com>
2020-11-10 13:10:49 -0800

    solved

---
+++ 1603.Design_Parking_System.swift
@@ -0,0 +1,76 @@
+// Solved by Xecca
+
+//1603. Design Parking System
+//Сomplexity: Easy
+//https://leetcode.com/problems/design-parking-system/
+
+//----------------------------------------------------
+//Runtime: 164 ms, faster than 90.18% of Swift online submissions for Design Parking System.
+//Memory Usage: 14.9 MB, less than 85.71% of Swift online submissions for Design Parking System.
+//----------------------------------------------------
+
+//Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+//
+//Implement the ParkingSystem class:
+//
+//ParkingSystem(int big, int medium, int small) Initializes object of the ParkingSystem class. The number of slots for each parking space are given as part of the constructor.
+//bool addCar(int carType) Checks whether there is a parking space of carType for the car that wants to get into the parking lot. carType can be of three kinds: big, medium, or small, which are represented by 1, 2, and 3 respectively. A car can only park in a parking space of its carType. If there is no space available, return false, else park the car in that size space and return true.
+//
+//Constraints:
+//
+//0 <= big, medium, small <= 1000
+//carType is 1, 2, or 3
+//At most 1000 calls will be made to addCar
+
+//----------------------------------------------------
+
+class ParkingSystem {
+    var parkingPlace = [0, 0, 0]
+    
+    init(_ big: Int, _ medium: Int, _ small: Int) {
+        parkingPlace[0] += big.self
+        parkingPlace[1] += medium.self
+        parkingPlace[2] += small.self
+    }
+    
+    func addCar(_ carType: Int) -> Bool {
+        
+        if parkingPlace[carType - 1] > 0 {
+            parkingPlace[carType - 1] -= 1
+            return true
+        }
+        
+        return false
+    }
+}
+
+//Your ParkingSystem object will be instantiated and called as such:
+let big = 1
+let medium = 1
+let small = 0
+let obj = ParkingSystem(big, medium, small)
+let ret_1: Bool = obj.addCar(1)
+let ret_2: Bool = obj.addCar(2)
+let ret_3: Bool = obj.addCar(3)
+let ret_4: Bool = obj.addCar(1)
+
+//Example 1:
+//
+//Input
+//["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+//[[1, 1, 0], [1], [2], [3], [1]]
+//Output
+//[null, true, true, false, false]
+//
+//Explanation
+//ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+//parkingSystem.addCar(1); // return true because there is 1 available slot for a big car
+//parkingSystem.addCar(2); // return true because there is 1 available slot for a medium car
+//parkingSystem.addCar(3); // return false because there is no available slot for a small car
+//parkingSystem.addCar(1); // return false because there is no available slot for a big car. It is already occupied.
+
+//Test cases:
+//
+
+
+
