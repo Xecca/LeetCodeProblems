4e62a71212d9055c289c7b0b02eff706a0c57d2d
Andrew Po <ensimgen@gmail.com>
2022-01-01 02:36:43 +0300

    Other solution (not mine).

--- 12.IntegerToRoman.swift
+++ 12.IntegerToRoman.swift
@@ -114,6 +114,23 @@ func generateRoman(_ strNum: String) -> String {
     return finalStr
 }
 
+//Other solution:
+
+//func intToRoman(_ num: Int) -> String {
+//  var integerValue = num
+//  var response = ""
+//
+//  let mappingList: [(Int, String)] = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+//
+//  for i in mappingList {
+//      while (integerValue >= i.0) {
+//          integerValue -= i.0
+//          response += i.1
+//      }
+//  }
+//  return response
+//}
+
 //Example 1:
 //
 //Input: num = 3
