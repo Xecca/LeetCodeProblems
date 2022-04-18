f5fdfa62430e615c84915f66f840739ed4041b14
Andrew Po <ensimgen@gmail.com>
2022-04-18 23:34:52 +0300

    Solved.

---
+++ 2239.FindClosestNumberToZero.swift
@@ -0,0 +1,60 @@
+// Solved by Xecca
+
+import Foundation
+
+//2239. Find Closest Number to Zero
+//Difficult: Easy
+//https://leetcode.com/problems/find-closest-number-to-zero/
+
+//Runtime: 159 ms, faster than 32.14% of Swift online submissions for Find Closest Number to Zero.
+//Memory Usage: 14 MB, less than 67.86% of Swift online submissions for Find Closest Number to Zero.
+
+//
+//Given an integer array nums of size n, return the number with the value closest to 0 in nums. If there are multiple answers, return the number with the largest value.
+//
+//Constraints:
+//
+//1 <= n <= 1000
+//-10^5 <= nums[i] <= 10^5
+
+//
+
+func findClosestNumber(_ nums: [Int]) -> Int {
+    var closestNum: Int = nums[0]
+    
+    for num in nums {
+        if abs(num) < abs(closestNum) {
+            closestNum = num
+        }
+    }
+    if nums.contains(abs(closestNum)) {
+        return abs(closestNum)
+    }
+    
+    return closestNum
+}
+
+//Example 1:
+//
+//Input: nums = [-4,-2,1,4,8]
+//Output: 1
+//Explanation:
+//The distance from -4 to 0 is |-4| = 4.
+//The distance from -2 to 0 is |-2| = 2.
+//The distance from 1 to 0 is |1| = 1.
+//The distance from 4 to 0 is |4| = 4.
+//The distance from 8 to 0 is |8| = 8.
+//Thus, the closest number to 0 in the array is 1.
+//Example 2:
+//
+//Input: nums = [2,-1,1]
+//Output: 1
+//Explanation: 1 and -1 are both the closest numbers to 0, so 1 being larger is returned.
+
+//Test cases:
+
+let input = [-4,-2,1,4,8]
+let answer = 1
+
+if findClosestNumber(input) == answer { print("Correct!") } else { print("Error! Expexted: \(answer)") }
+
